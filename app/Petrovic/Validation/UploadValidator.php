8e19085dbe20010024b53454bd9ba0e98a20d05f
vagrant <vagrant@precise32.(none)>
2014-04-19 16:38:49 +0000

    =add image format validation

--- app/Petrovic/Validation/UploadValidator.php
+++ app/Petrovic/Validation/UploadValidator.php
@@ -5,6 +5,8 @@ class UploadValidator{
 
 	protected $errors;
 
+	protected $allowedTypes = ['jpg', 'jpeg', 'png', 'gif', 'bmp'];
+
 	/**
 	 * validate multiple Upload
 	 * @param  array $upload
@@ -25,6 +27,13 @@ class UploadValidator{
 				if( ! $item->isValid() ) 
 				{
 					$this->errors = $item->getErrorMessage();
+					return false;
+				}
+
+				if( ! in_array($item->getClientMimeType(), $this->allowedTypes)  )
+				{
+					$this->errors = "Unsupported file format. 
+						Upload only 'jpg', 'jpeg', 'png', 'gif', 'bmp' image formats.";
 					return false;
 				}
 			}
